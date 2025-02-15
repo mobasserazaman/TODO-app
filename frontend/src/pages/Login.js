48f9f73212f466dff5dbff2bae0d00c4e10dd110
Mobassera Zaman <mobassera.zaman@mail.mcgill.ca>
2025-02-14 23:24:17 -0500

    Added some MUI to Login page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import { replace, useLocation, useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { login, register } from '../slices/authSlice';
+import { Button, FormControl, InputLabel, OutlinedInput, TextField, Box, Typography } from "@mui/material";
+
 
 export default function Login() {
 
@@ -31,22 +33,42 @@ export default function Login() {
 
     return (
         <div>
-            <div><h1>TODOs</h1></div>
-            <form onSubmit={handleLogin}>
-                <label>Username:</label>
-                <input type='text' id='username' value={username} onChange={e => setUsername(e.target.value)} required></input>
-                <label>Password:</label>
-                <input type='password' id='password' value={password} onChange={e => setPassword(e.target.value)} required></input>
-                <button type='submit'>Login</button>
-            </form>
-
-            <form onSubmit={handleRegister}>
-                <label>Username:</label>
-                <input type='text' id='username2' value={username2} onChange={e => setUsername2(e.target.value)} required></input>
-                <label>Password:</label>
-                <input type='password' id='password2' value={password2} onChange={e => setPassword2(e.target.value)} required></input>
-                <button type='submit'>Register</button>
-            </form>
+            <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", width: "70%", margin: "0 auto" }}>
+                <Typography variant='h4'>TODO List App</Typography>
+                <Typography variant="body2">
+                    Created by <strong>Mobassera Zaman</strong>
+                </Typography>
+                <Box sx={{ width: "35%", margin: "0 auto" }}>
+                    <FormControl sx={{ margin: "10px" }} fullWidth required>
+                        <InputLabel>Username</InputLabel>
+                        <OutlinedInput id="username" value={username} onChange={e => setUsername(e.target.value)}></OutlinedInput>
+                    </FormControl>
+                    <FormControl sx={{ margin: "10px" }} fullWidth required>
+                        <InputLabel>Password</InputLabel>
+                        <OutlinedInput id="password" value={password} onChange={e => setPassword(e.target.value)}></OutlinedInput>
+                    </FormControl>
+                    <Box sx={{ display: "flex", justifyContent: "center" }}>
+                        <Button variant="contained" color="primary" type='submit' onClick={handleLogin}>
+                            Login
+                        </Button>
+                    </Box>
+                </Box>
+                <Box sx={{ width: "35%", margin: "0 auto" }}>
+                    <FormControl sx={{ margin: "10px" }} fullWidth required>
+                        <InputLabel>Username</InputLabel>
+                        <OutlinedInput id="username2"value={username2} onChange={e => setUsername2(e.target.value)} required ></OutlinedInput>
+                    </FormControl>
+                    <FormControl sx={{ margin: "10px" }} fullWidth required>
+                        <InputLabel>Password</InputLabel>
+                        <OutlinedInput id="password2" value={password2} onChange={e => setPassword2(e.target.value)} required></OutlinedInput>
+                    </FormControl>
+                    <Box sx={{ display: "flex", justifyContent: "center" }}>
+                        <Button variant="contained" color="primary" type='submit' onClick={handleRegister}>
+                            Register
+                        </Button>
+                    </Box>
+                </Box>
+            </Box>
         </div>
     )
 }
